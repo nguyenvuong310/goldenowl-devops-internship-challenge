ea368b888a78ad96aef35967f618a14803b1ce22
nguyenvuong310 <trungvuong2169@gmail.com>
2025-01-04 12:45:43 +0700

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = Router()
 
 router.get('/', (req, res) => {
     const responseJson = {
-        message: 'Welcome warriors to Golden Owl!',
+        message: 'Welcome warriors to Golden Owl!1',
     }
     res.json(responseJson)
 })
