01cabda9c9b713362ae07c42cf69ceffb0ffab76
nguyenvuong310 <trungvuong2169@gmail.com>
2025-01-04 10:07:52 +0700

    fix workflow

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = Router()
 
 router.get('/', (req, res) => {
     const responseJson = {
-        message: 'Welcome warriors to Golden Owl!!',
+        message: 'Welcome warriors to Golden Owl!',
     }
     res.json(responseJson)
 })
